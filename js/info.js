76b07feb23b87ce9f11df639c0140ae36b129563
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2017-02-24 16:24:05 -0800

    Update info.js and operatiors.js

---
+++ js/info.js
@@ -0,0 +1,46 @@
+/*
+	JavaScript (JS) is a lightweight, interpreted, programming language 
+	with first-class functions. While it is most well-known as the scripting 
+	language for Web pages, many non-browser environments also use it, such as 
+	node.js and Apache CouchDB. JS is a prototype-based, multi-paradigm, 
+	dynamic scripting language, supporting object-oriented, imperative, 
+	and declarative (e.g. functional programming) styles.
+*/
+
+/****** Comments ******/
+
+// Inline comments done by using double slash 
+
+/* MultiLine comments done by using 
+a pair of openning slash plus star 
+proceeded by comments block and 
+closing star plus slash
+*/
+
+/****** Data Types and Values ******/
+/*
+	The Undefined Type - undefined
+	The Null Type - null
+	The Boolean Type - true, false
+	The String Type - "sone string"
+	The Symbol Type - @@toPrimitive
+	The Number Type - 4, 45, 2.1, NaN, positive Infinity and negative Infinity
+	
+	The Object Type - {}, Array, Object, 
+
+*/
+
+/****** Semantics ******/
+/*
+	do Statement while(Expression);
+	while (Expression) Statement
+	for( Expressionopt;Expressionopt;Expressionopt)Statement
+	for (var VariableDeclarationList; Expressionopt; Expressionopt) Statement
+	for (LexicalDeclaration Expressionopt; Expressionopt)Statement
+	for (LeftHandSideExpression in Expression)Statement
+	for (varForBinding in Expression)Statement
+	for (ForDeclaration in Expression)Statement
+	for (LeftHandSideExpression of AssignmentExpression)Statement
+	for (var ForBinding of AssignmentExpression)Statement
+	for (ForDeclaration of AssignmentExpression)Statement
+*/
---
+++ js/operators.js
@@ -0,0 +1,92 @@
+/*
+1.  Arithmetic operators
+2.  Assignment operators
+3.  String operators
+4.  Comparasion operators 
+5.  Logical operators
+6.  Type operators
+7.  Bitwise operators
+*/
+
+/****** Arithmetic operators ******/
+/*
+	Operator	Description
+	+			Addition
+	-			Subtraction
+	*			Multiplication
+	/			Division
+	%			Modulus
+	++			Increment
+	--			Decrement
+*/
+
+/****** Assignment operators ******/
+/*
+	Operator	Example		Same As
+	=			x = y		x = y
+	+=			x += y		x = x + y
+	-=			x -= y		x = x - y
+	*=			x *= y		x = x * y
+	/=			x /= y		x = x / y
+	%=			x %= y		x = x % y
+*/
+
+/****** String operators ******/
+
+/*
+	txt1 = "John";
+	txt2 = "Doe";
+	txt3 = txt1 + " " + txt2; => "John Doe"
+
+	txt1 = "What a very ";
+	txt1 += "nice day"; => "What a very nice day"
+
+	x = 5 + 5; => "10"
+	y = "5" + 5; => "55"
+	z = "Hello" + 5; => "Hello5"
+*/
+
+/****** Comparasion operators ******/
+
+/*
+	Operator	Description
+	==			equal to
+	===			equal value and equal type
+	!=			not equal
+	!==			not equal value or not equal type
+	>			greater than
+	<			less than
+	>=			greater than or equal to
+	<=			less than or equal to
+	?			ternary operator
+*/
+
+/****** Logical operators ******/
+
+/*
+	Operator	Description
+	&&			logical and
+	||			logical or
+	!			logical not
+*/
+
+/****** Type operators ******/
+
+/*
+	Operator	Description
+	typeof		Returns the type of a variable
+	instanceof	Returns true if an object is an instance of an object type
+*/
+
+/****** Bitwise operators ******/
+
+/*
+	Operator	Description				Example		Same as			Result	Decimal
+	&			AND						5 & 1		0101 & 0001		0001	 1
+	|			OR						5 | 1		0101 | 0001		0101	 5
+	~			NOT						~ 5	 		~0101			1010	 10
+	^			XOR						5 ^ 1		0101 ^ 0001		0100	 4
+	<<			Zero fill left shift	5 << 1		0101 << 1		1010	 10
+	>>			Signed right shift		5 >> 1		0101 >> 1		0010	  2
+	>>>			Zero fill right shift	5 >>> 1		0101 >>> 1		0010	  2
+*/
